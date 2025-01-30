cfd5a78c99fe653fc76c7ca38e4d042b48c8062d
Helena <helenagp05@gmail.com>
2025-01-30 13:56:18 +0100

    component header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import LogoutButton from '../logout/LogoutButton';
+import styles from "../styles/header.module.css"
 
 export default function Header() {
   const router = useRouter();
@@ -8,23 +8,23 @@ export default function Header() {
   const isActive = (paths) => paths.includes(router.pathname);
 
     return (
-        <header className="header">
-            <div className="header-logo">
+        <header className={styles.header}>
+            <div className={styles.headerLogo}>
                 <Link href="/home" style={{ textDecoration: 'none', color: 'inherit' }} passHref>
-                    <img src="/assets/logo.png" alt="WizarlyWonders Logo" />
+                    <img src="/assets/logo.png" alt="WizarlyWonders Logo" width={30}
+                        height={30}/>
                 </Link>
                 <Link href="/home" style={{ textDecoration: 'none', color: 'inherit' }} passHref>
-                    <span className="header-title">Wizarly Wonders</span>
+                    <span className={styles.headerTitle}>Wizarly Wonders</span>
                 </Link>
             </div>
-            <nav className="header-nav">
+            <nav className={styles.headerNav}>
                 <span>Spell Encyclopedia</span>
                 <span>House Sorting Quiz</span>
                 <span>Character Journal</span>
                 <span>Wand Generator</span>
                 <span>Howarts Legacy Hub</span>
-            </nav>
-            <LogoutButton/>     
+            </nav>   
         </header>
     );
   }
